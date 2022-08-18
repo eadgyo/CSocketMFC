44495cf5fe674d15a6cf666ff289cfaa088452db
eadgyo <eadgyo@gmail.com>
2022-08-18 23:15:14 +0200

    Correction return false throw

--- include/debug.h
+++ include/debug.h
@@ -7,4 +7,4 @@
 #define LOG_INFO(val) if(LOG_LEVEL_INFO) std::cout << "LOG_INFO::" << __FUNCTION__ << "()-> " << val <<  std::endl;
 #define LOG_TRACE(val) if(LOG_LEVEL_TRACE) std::cout << "LOG_TRACE::" << __FUNCTION__ << "()-> " << val <<  std::endl;
 #define LOG_DEBUG(val) if(LOG_LEVEL_DEBUG) std::cout << "LOG_DEBUG::" << __FUNCTION__ << "()-> " << val <<  std::endl;
-#define THROW_IF(val) if (val) std::cerr << "Failed to " << __FUNCTION__ << "()-> " << strerror(errno) << std::endl; return false; 
+#define THROW_IF(val) if (val) { std::cerr << "Failed to " << __FUNCTION__ << "()-> " << strerror(errno) << std::endl; return false; }
